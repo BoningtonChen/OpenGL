80ec5f54c31214197a98b6d9c25a24d33be57ba2
Bonington Chen <yikun.chen@163.com>
2024-06-24 21:53:44 +0800

    Abstract Vertex Shader & Fragment Shader into the .shader file

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -2,6 +2,51 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+struct ShaderProgramSource
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+
+    enum class ShaderType
+    {
+	    NONE = -1,
+        VERTEX = 0,
+        FRAGMENT = 1
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+
+    while (getline(stream, line))
+    {
+	    if (line.find("#shader") != std::string::npos)
+	    {
+            if (line.find("vertex") != std::string::npos)
+                type = ShaderType::VERTEX;
+            else if (line.find("fragment") != std::string::npos)
+                type = ShaderType::FRAGMENT;
+	    }
+	    else
+	    {
+            ss[static_cast<int>(type)] << line << '\n';
+	    }
+    }
+
+    return {
+    	ss[0].str(),
+    	ss[1].str()
+    };
+}
 
 static unsigned int CompileShader(unsigned int type, const std::string& source)
 {
@@ -16,15 +61,17 @@ static unsigned int CompileShader(unsigned int type, const std::string& source)
     {
         int length;
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
-        char* message = static_cast<char*>(alloca(length * sizeof(char)));
+        char* message = static_cast<char*>(
+            alloca(length * sizeof(char))
+            );
         glGetShaderInfoLog(id, length, &length, message);
 
         std::cout
     	<< "Fail to compile "
     	<< (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
     	<< " shader!"
-    	<< std::endl;
-        std::cout << message << std::endl;
+    	<< "\n";
+        std::cout << message << "\n";
 
         glDeleteShader(id);
 
@@ -101,32 +148,12 @@ int main(void)
         GL_FALSE, 
         sizeof(float) * 2,
         nullptr
-        );
+        ); 
 
-    std::string vertexShader = 
-        "#version 330 core\n"
-        "\n"
-		"layout(location = 0)in vec4 position;\n"
-		"\n"
-		"void main()\n"
-		"{\n"
-		"   gl_Position = position;\n"
-		"}\n"
-	;
+    ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
 
-    std::string fragmentShader =
-        "#version 330 core\n"
-        "\n"
-        "layout(location = 0)out vec4 color;\n"
-        "\n"
-        "void main()\n"
-        "{\n"
-        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
-        "}\n"
-        ;
-
-    unsigned int shader = CreateShader(vertexShader, fragmentShader);
-    glUseProgram(shader);
+    unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
+	glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -142,6 +169,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
