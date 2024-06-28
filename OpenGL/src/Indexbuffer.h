77f14ebf331b8d819948ce58210ef6fe362a113f
Bonington Chen <yikun.chen@163.com>
2024-06-28 09:23:38 +0800

    Abstract Renderer System.

--- OpenGL/src/Indexbuffer.h
+++ OpenGL/src/Indexbuffer.h
@@ -8,9 +8,10 @@ public:
 
 	void Bind() const;
 	void Unbind() const;
+
+	inline unsigned int GetCount() const { return m_Count; }
+
 private:
 	unsigned int m_RendererID;
 	unsigned int m_Count;
-
-	inline unsigned int GetCount() const { return m_Count; }
 };
