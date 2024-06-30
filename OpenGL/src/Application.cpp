dc33f4f3f897fad1cc6c8ef4198b177ea66053ab
Bonington Chen <yikun.chen@163.com>
2024-06-30 10:18:30 +0800

    Merge in matrix.

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -15,6 +15,9 @@
 #include "Shader.h"
 #include "Texture.h"
 
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
 int main(void)
 {
 	/* Initialize the library */
@@ -78,11 +81,14 @@ int main(void)
 
         IndexBuffer ib(indices, 6);
 
+        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
+        shader.SetUniformMat4f("u_MVP", proj);
 
-        Texture texture("res/textures/Bonington_Logo.png");
+        Texture texture("res/textures/Bonington_Logo_white.png");
         texture.Bind();
         shader.SetUniform1i("u_Texture", 0);
 
